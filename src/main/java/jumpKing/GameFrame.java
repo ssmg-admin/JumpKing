4070b4c95b189fe3ae0c2287aead8451c3ad488b
ssmg-admin <github@ssmg.cz>
2026-02-09 14:24:49 +0100

    Update GameFrame.java

--- src/main/java/jumpKing/GameFrame.java
+++ src/main/java/jumpKing/GameFrame.java
@@ -2,6 +2,8 @@ package jumpKing;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 
 /**
  * GameFrame je hlavní herní panel.
@@ -11,7 +13,10 @@ import java.awt.*;
  * - vstup z klávesnice
  * - vykreslování herních objektů
  */
-class GameFrame extends JPanel {
+class GameFrame extends JPanel implements MouseListener {
+
+    Rectangle player = new Rectangle(400,400,64,64);
+    Rectangle platrofma =  new Rectangle(0,200,300,20);
 
     /**
      * Konstruktor GameFrame:
@@ -20,6 +25,7 @@ class GameFrame extends JPanel {
      */
     public GameFrame() {
 
+        addMouseListener(this);
         // Panel musí být fokusovatelný, aby mohl přijímat klávesy
         setFocusable(true);
 
@@ -28,6 +34,10 @@ class GameFrame extends JPanel {
          * Timer volá anonymní funkci (lambda) e -> { ... }
          */
         new Timer(16, e -> {
+
+            if (player.y < 500){
+                player.y = player.y+5;
+            }
 
 
             repaint();
@@ -42,6 +52,35 @@ class GameFrame extends JPanel {
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
+        g.setColor(Color.RED);
+        g.fillRect(platrofma.x, platrofma.y, platrofma.width, platrofma.height);
+        g.setColor(Color.GREEN);
+        g.fillRect(player.x, player.y, player.width, player.height);
+
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+        player.y = player.y - 100;
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
 
     }
 }
